79af1036f8a97c0aeea94d155b9e9e273c421bfe
Tuanvu0809 <164357111+Tuanvu0809@users.noreply.github.com>
2025-07-15 21:03:38 +0700

    Delete design_parten directory

--- design_parten/Creational/Factory/light/light.h
+++
@@ -1,9 +0,0 @@
-#ifndef LIGHT_H
-#define LIGHT_H
-    #include "../sensor/sensor.h"
-
-    void init_light();
-    float data_light();
-    sensor creat_light();
-
-#endif
--- design_parten/Structral/Decorate/communicate/CRC.h
+++
@@ -1,8 +0,0 @@
-#ifndef CRC_H
-#define CRC_H
-    #include "communicate.h"
-    typedef struct {
-        Channel* inner;
-    } CRCChannel;
-    Channel create_crc_channel(Channel* inner);
-#endif
